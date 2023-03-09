ae70c3bf0a2d7f47f4f3917bc9c30036b6075fa2
Rishabhkushwaha17 <Rishabh.kushwaha@chicmic.co.in>
2023-03-09 12:41:02 +0530

    added collision and physics

--- assets/Script/PlayerMovement.ts
+++ assets/Script/PlayerMovement.ts
@@ -1,22 +1,37 @@
-import { _decorator, Component, Node, UITransform, Vec3, Vec2, Animation, ImageAsset } from "cc";
+import { _decorator, Component, Node, UITransform, Vec3, Vec2, Animation, ImageAsset, SpriteFrame, Sprite, PhysicsSystem, PhysicsSystem2D, AudioClip, AudioSourceComponent, Collider2D, Contact2DType, IPhysics2DContact, Camera, log } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("PlayerMovement")
 export class PlayerMovement extends Component {
-    @property({ type: ImageAsset })
-    playerImage: ImageAsset = null;
+    @property({ type: SpriteFrame })
+    playerImage: SpriteFrame = null;
     @property({ type: Node })
     joystick: Node = null;
     @property({ type: Node })
     joyStickBall: Node = null;
+    @property({ type: Node })
+    camera: Node = null;
+    @property({ type: Node })
+    hud: Node = null;
     pos: Vec2 = null;
     startPos: Vec3 = null;
-    stopPlayer: boolean = true;
     intialPos: Vec3 = null;
     finalPosBall: Vec3 = null;
     finalPos: Vec3 = null;
+    canMovePlayer: boolean = false;
+    collided: boolean = false;
+    collisionangle: number = null
+    playerSpeed: number = 0.04
+    touchenabled: boolean = true;
+    count: number = 0;
+    anlges: number = 0
     start() {
         this.touchEventsFunc();
+        PhysicsSystem2D.instance.enable = true;
+        if (PhysicsSystem2D.instance) {
+            PhysicsSystem2D.instance.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+            PhysicsSystem2D.instance.on(Contact2DType.END_CONTACT, () => { this.collided = false; }, this);
+        }
     }
     onLoad() {
         this.finalPosBall = new Vec3(1, 1, 0);
@@ -24,97 +39,127 @@ export class PlayerMovement extends Component {
         this.intialPos = new Vec3(1, 1, 0);
         this.startPos = this.joyStickBall.getPosition();
     }
+    onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        console.log(selfCollider.node.name);
+        if (otherCollider.node.name == "player") {
+            this.collided = true;
+        }
+    }
     touchEventsFunc() {
         this.joyStickBall.on(Node.EventType.TOUCH_START, this.touchStart, this);
-        this.joyStickBall.on(Node.EventType.TOUCH_MOVE, this.touchMove, this);
+        this.joyStickBall.on(Node.EventType.TOUCH_MOVE, (e) => {
+            if (this.touchenabled) {
+                this.touchMove(e)
+            }
+            else {
+                this.touchEnd();
+            }
+        }, this);
+
         this.joyStickBall.on(Node.EventType.TOUCH_END, this.touchEnd, this);
         this.joyStickBall.on(Node.EventType.TOUCH_CANCEL, this.touchEnd, this);
     }
     touchEnd() {
-        this.stopPlayer = false;
-        //console.log("end");
+        this.canMovePlayer = false;
         this.joyStickBall.setPosition(0, 0, 0);
     }
     touchStart() {
         this.joyStickBall.setPosition(0, 0, 0);
-        //console.log("start");
+        this.touchenabled = true;
+        this.canMovePlayer = true;
     }
     touchMove(e) {
-        this.stopPlayer = true;
-        this.intialPos = this.joyStickBall.parent
+        let first_touch = this.hud
             .getComponent(UITransform)
-            .convertToNodeSpaceAR(new Vec3(e.getUILocation().x, e.getUILocation().y, e.getUILocation().z));
-        // var finalPosBall: Vec3;
-        var len = this.intialPos.length();
-        //console.log("len", len);
-        var joyStickBallBaseWidth = this.joyStickBall.parent.getComponent(UITransform).getBoundingBox().width / 2;
-        if (len > joyStickBallBaseWidth) {
-            this.intialPos.x = (this.intialPos.x * joyStickBallBaseWidth) / len;
-            this.intialPos.y = (this.intialPos.y * joyStickBallBaseWidth) / len;
+            .convertToNodeSpaceAR(new Vec3(e.getUILocation().x, e.getUILocation().y, e.getUILocation().z))
+        let final_touch = new Vec2(this.startPos.x - first_touch.x, this.startPos.y - first_touch.y)
+        let width_check = new Vec3(this.hud.parent.getComponent(UITransform).width / 2 - this.hud.getComponent(UITransform).width / 2, this.hud.parent.getComponent(UITransform).height / 2 - this.hud.getComponent(UITransform).width / 2, 0)
+        if (final_touch.length() > width_check.length()) {
+            this.touchenabled = false
+            this.touchEnd();
         }
-
-        var dy = this.intialPos.y;
-        var dx = this.intialPos.x;
-        var angleRad = Math.atan2(dy, dx);
-        var angleDeg = (angleRad * 180) / Math.PI;
-        //console.log("angle ", angleDeg, angleRad);
-        this.getDirection(angleDeg);
-        this.joyStickBall.setPosition(this.intialPos);
+        if (!this.collided) {
+            this.intialPos = this.hud
+                .getComponent(UITransform)
+                .convertToNodeSpaceAR(new Vec3(e.getUILocation().x, e.getUILocation().y, e.getUILocation().z));
+            var len = this.intialPos.length();
+            var joyStickBallBaseWidth = this.hud.getComponent(UITransform).getBoundingBox().width / 2;
+            if (len > joyStickBallBaseWidth) {
+                this.intialPos.x = (this.intialPos.x * joyStickBallBaseWidth) / len;
+                this.intialPos.y = (this.intialPos.y * joyStickBallBaseWidth) / len;
+            }
+            var dy = this.intialPos.y;
+            var dx = this.intialPos.x;
+            var angleRad = Math.atan2(dy, dx);
+            var angleDeg = (angleRad * 180) / Math.PI;
+            this.getDirection(angleDeg);
+            this.handlecollision(angleDeg);
+            this.joyStickBall.setPosition(this.intialPos);
+            if (angleDeg < 0) {
+                this.anlges = angleDeg + 360;
+            }
+            else {
+                this.anlges = angleDeg;
+            }
+        }
     }
     getDirection(angle) {
-        if (angle > -15 && angle < 15) {
-            this.playWalkAnmation("East");
-        } else if (angle > 15 && angle < 75) {
-            this.playWalkAnmation("North_East");
-        } else if (angle > 75 && angle < 105) {
-            this.playWalkAnmation("North");
-        } else if (angle > 105 && angle < 165) {
-            this.playWalkAnmation("North_West");
-        } else if (angle > 165 && angle < 195) {
-            this.playWalkAnmation("West");
-        } else if (angle > 195 && angle < 255) {
-            this.playWalkAnmation("South_West");
-        } else if (angle > 255 && angle > 285) {
-            this.playWalkAnmation("South");
-        } else {
+        if (this.anlges < 90 || this.anlges > 270) {
             this.playWalkAnmation("South_East");
+        } else {
+            this.playWalkAnmation("West");
         }
+    }
+    handlecollision(angle) {
+
+
+
+        console.log(this.collisionangle);
+
+
+        if (this.anlges < (this.collisionangle - 25) || this.anlges > (this.collisionangle + 25)) {
+            this.playerSpeed = 0.04
+            console.log("abbb");
+            console.log(this.count);
+            this.count = 0;
+            this.collided = false
+        }
+
+
+
+
+    }
+    enablemovement(angle, lower_number, upper_number, middle_number) {
     }
     playWalkAnmation(walkDirection: String) {
         switch (walkDirection) {
             case "North":
                 {
-                    //console.log(walkDirection);
                     this.node.getComponent(Animation)?.play("north");
                 }
                 break;
             case "East":
                 {
-                    //console.log(walkDirection);
                     this.node.getComponent(Animation)?.play("east");
                 }
                 break;
             case "South":
                 {
-                    //console.log(walkDirection);
                     this.node.getComponent(Animation)?.play("south");
                 }
                 break;
             case "West":
                 {
-                    //console.log(walkDirection);
                     this.node.getComponent(Animation)?.play("west");
                 }
                 break;
             case "North_East":
                 {
-                    //console.log(walkDirection);
                     this.node.getComponent(Animation)?.play("northEast");
                 }
                 break;
             case "North_West":
                 {
-                    //console.log(walkDirection);
                     this.node.getComponent(Animation)?.play("northWest");
                 }
                 break;
@@ -134,17 +179,41 @@ export class PlayerMovement extends Component {
     }
     changeSprite() { }
     update(deltaTime: number) {
-        if (this.stopPlayer) {
+        if (this.canMovePlayer) {
+
+
             this.pos = new Vec2(this.startPos.x - this.intialPos.x, this.startPos.y - this.intialPos.y);
-            this.finalPos.x -= this.pos.x * 0.01;
-            this.finalPos.y -= this.pos.y * 0.01;
+            if (this.collided) { }
+            if (!this.collided) {
+                this.finalPos.x -= this.pos.x * this.playerSpeed;
+                this.finalPos.y -= this.pos.y * this.playerSpeed;
+            }
+            else {
+                this.finalPos = this.node.getPosition();
+            }
             this.pos.x = 0;
             this.pos.y = 0;
         }
-
         let playerPosition = new Vec3();
         let currentPostion: Vec3 = this.node.getPosition();
+        if (this.collided && this.count == 0) {
+            console.log(this.playerSpeed);
+            if ((this.anlges < 295 && this.anlges > 245) || (this.anlges < 25 || this.anlges > 345) || (this.anlges > 75 && this.anlges < 115) || (this.anlges > 155 && this.anlges < 215)) {
+                this.collisionangle = this.anlges;
+
+                this.playerSpeed = 0;
+                this.count = 1;
+                console.log(1);
+            }
+
+        }
+        if (this.collided) {
+            this.finalPos = currentPostion; this.pos = new Vec2(0, 0); this.collided = false; console.log(this.canMovePlayer, "hell ya");
+
+        }
         Vec3.lerp(playerPosition, currentPostion, this.finalPos, 0.06);
         this.node.setPosition(playerPosition);
+        this.camera.setPosition(playerPosition);
+
     }
 }
--- assets/Script/wallscollisions.ts
+++ assets/Script/wallscollisions.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, TiledMap, PhysicsSystem2D, Contact2DType, Collider2D, IPhysics2DContact, Rect, UITransform, TiledObjectGroup, Vec3 } from 'cc';
+import { _decorator, Component, Node, TiledMap, PhysicsSystem2D, Contact2DType, Collider2D, IPhysics2DContact, Rect, UITransform, TiledObjectGroup, Vec3, TiledLayer, TiledTile, RigidBody, RigidBody2D, RigidBodyComponent, BoxCollider2D, Vec2, ERigidBody2DType, PhysicsSystem, EPhysics2DDrawFlags, director } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('walls')
@@ -6,29 +6,53 @@ export class walls extends Component {
     @property({ type: Node })
     player: Node = null
     player_bb: Rect
+    map: any
     onLoad() {
 
 
 
-        console.log(this.node.getComponent(TiledMap).getObjectGroups())
 
+
+        PhysicsSystem2D.instance.enable = true;
+        this.map = this.node.getComponent(TiledMap)
+        console.log(this.node.getComponent(TiledMap).getObjectGroups())
 
 
     }
     start() {
+        let layer: TiledLayer = this.map.getLayer("Tile Layer 2")
+        layer.getComponent(UITransform).setAnchorPoint(0, 0);
+        let tilesize = layer.getMapTileSize();
+        for (let i = 0; i < layer.getLayerSize().width; i++) {
+            for (let j = 0; j < layer.getLayerSize().height; j++) {
+                let tile: TiledTile = layer.getTiledTileAt(i, j, true);
+                if (tile.grid != 0) {
+                    tile.addComponent(RigidBody2D);
+                    tile.getComponent(RigidBody2D).type = ERigidBody2DType.Static;
+                    tile.getComponent(RigidBody2D).allowSleep = false;
+                    tile.getComponent(RigidBody2D).awakeOnLoad = true;
+                    tile.getComponent(RigidBody2D).gravityScale = 0;
+                    let collider = tile.addComponent(BoxCollider2D);
+                    collider.size = tilesize;
+                    collider.offset = new Vec2(tilesize.width / 2, tilesize.height / 2)
+                    collider.apply();
+                }
+            }
+        }
+
     }
 
     update(deltaTime: number) {
 
 
-        this.player_bb = this.player.getComponent(UITransform).getBoundingBoxToWorld();
-        let b: Rect = new Rect(this.player_bb.x, 0, 32, 32);
-        let aa: any = this.node.getComponent(TiledMap).getObjectGroups()[0].getObjects()[0];
-        let a: Rect = new Rect(aa.x, 0, 617, 452);
-        // let a: Rect = new Rect(this.node.getComponent(TiledMap).getObjectGroups()[0].getObjects()[0].x, this.node.getComponent(TiledMap).getObjectGroups()[0].getObjects()[0].y, 32, 32)
-        if (this.player_bb.intersects(a)) {
-            console.log("lad gaya");
-        }
+        // this.player_bb = this.player.getComponent(UITransform).getBoundingBoxToWorld();
+        // let b: Rect = new Rect(this.player_bb.x, 0, 32, 32);
+        // let aa: any = this.node.getComponent(TiledMap).getObjectGroups()[1].getObjects()[0];
+        // let a: Rect = new Rect(aa.x, 0, 617, 452);
+        // // let a: Rect = new Rect(this.node.getComponent(TiledMap).getObjectGroups()[0].getObjects()[0].x, this.node.getComponent(TiledMap).getObjectGroups()[0].getObjects()[0].y, 32, 32)
+        // if (this.player_bb.intersects(a)) {
+        //     console.log("lad gaya");
+        // }
 
         // if (this.player_bb.intersects(a)) {
         //     console.log("lad gaya");
