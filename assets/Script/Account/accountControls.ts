acb987039b0a994eec4c1d1c67bbb76470870f48
Mohit Malik <mohit.malik@chicmic.co.in>
2023-07-18 11:57:38 +0530

    Changes: Account and Setting prefab updated

--- assets/Script/Account/accountControls.ts
+++ assets/Script/Account/accountControls.ts
@@ -1,41 +1,45 @@
-import { _decorator, Component, Node, Canvas, Input, Label, EditBox, director } from 'cc';
-import { gameData } from '../singleton/gameData';
+import { _decorator, Component, Node, Canvas, Input, Label, EditBox, director, Button } from "cc";
+import { gameData } from "../singleton/gameData";
 const { ccclass, property } = _decorator;
 
-@ccclass('accountControls')
+@ccclass("accountControls")
 export class accountControls extends Component {
-    @property({ type: Node })
-    closeButton: Node = null;
+  @property({ type: Button })
+  closeButton: Button = null;
+  @property({ type: Node })
+  accountPanel: Node = null;
+  @property({ type: EditBox })
+  UserName: EditBox = null;
 
-    @property({ type: EditBox })
-    UserName: EditBox = null;
+  gameDataInstance = gameData.getInstance();
+  onLoad() {
+    this.UserName.getComponent(EditBox).string = this.gameDataInstance.GetSaveUserName();
+    // this.closeButton.on(Input.EventType.TOUCH_START, () => {
+    //   setTimeout(() => {
+    //     this.node.destroy();
+    //   });
+    // });
+  }
 
-    gameDataInstance = gameData.getInstance()
-    onLoad() {
-        this.UserName.getComponent(EditBox).string = this.gameDataInstance.GetSaveUserName()
-        this.closeButton.on(Input.EventType.TOUCH_START, () => {
-            setTimeout(() => {
-                this.node.destroy()
-            });
-        })
-    }
+  MakeUserNameFieldActive() {
+    // Make the editbox active on click of a button
+    this.UserName.focus();
+  }
 
-    MakeUserNameFieldActive() {
-        // Make the editbox active on click of a button
-        this.UserName.focus()
-    }
+  EditUserName() {
+    this.gameDataInstance.SetSaveUserName(this.UserName.getComponent(EditBox).string);
+    this.UserName.getComponent(EditBox).string = this.gameDataInstance.GetSaveUserName();
+  }
 
-    EditUserName() {
-        this.gameDataInstance.SetSaveUserName(this.UserName.getComponent(EditBox).string)
-        this.UserName.getComponent(EditBox).string = this.gameDataInstance.GetSaveUserName()
-    }
+  openAccountPopUp() {
+    this.accountPanel.active = true;
+  }
+  diableAccountPopUp() {
+    this.accountPanel.active = false;
+  }
+  start() {
+    console.log("Started");
+  }
 
-    start() {
-        console.log("Started");
-    }
-
-    update(deltaTime: number) {
-
-    }
+  update(deltaTime: number) {}
 }
-
--- assets/Script/Settings/VolumeAdjustment.ts
+++ assets/Script/Settings/VolumeAdjustment.ts
@@ -1,52 +1,58 @@
-import { _decorator, Component, Node, Slider, sys, Input } from 'cc';
-import { audioManager } from '../audio/scripts/audioManager';
+import { _decorator, Component, Node, Slider, sys, Input, Button } from "cc";
+import { audioManager } from "../audio/scripts/audioManager";
 // import { audioManager } from './audio/scripts/audioManager';
 const { ccclass, property } = _decorator;
 
-@ccclass('VolumeAdjustment')
+@ccclass("VolumeAdjustment")
 export class VolumeAdjustment extends Component {
-    @property({ type: Node })
-    MusicSlider: Node = null;
+  @property({ type: Node })
+  MusicSlider: Node = null;
+  @property({ type: Node })
+  settingPanel: Node = null;
+  @property({ type: Node })
+  SoundSlider: Node = null;
 
-    @property({ type: Node })
-    SoundSlider: Node = null;
+  @property({ type: Button })
+  closeButton: Button = null;
 
-    @property({ type: Node })
-    closeButton: Node = null;
+  audioInstance = audioManager.getInstance();
+  onLoad() {
+    console.log("Setting Panel loaded");
+    this.MusicSlider!.on("slide", this.adjustMusicSound);
+    this.SoundSlider!.on("slide", this.adjustSoundEffect);
 
-    audioInstance = audioManager.getInstance()
-    onLoad() {
-        this.MusicSlider!.on("slide", this.adjustMusicSound)
-        this.SoundSlider!.on("slide", this.adjustSoundEffect)
+    this.MusicSlider.getComponent(Slider).progress = this.audioInstance.getMusicSliderProgress();
+    this.SoundSlider.getComponent(Slider).progress = this.audioInstance.getSoundSliderProgress();
 
-        this.MusicSlider.getComponent(Slider).progress = this.audioInstance.getMusicSliderProgress()
-        this.SoundSlider.getComponent(Slider).progress = this.audioInstance.getSoundSliderProgress()
+    // this.closeButton.on(Input.EventType.TOUCH_START, () => {
+    //     setTimeout(() => {
+    //         this.node.destroy()
+    //     });
+    // })
+  }
 
-        this.closeButton.on(Input.EventType.TOUCH_START, () => {
-            setTimeout(() => {
-                this.node.destroy()
-            });
-        })
-    }
+  adjustMusicSound = () => {
+    let progress = this.MusicSlider.getComponent(Slider).progress;
+    sys.localStorage.setItem("MusicProgress", JSON.stringify(progress));
+    this.audioInstance.adjustMusicSound(progress);
+  };
 
-    adjustMusicSound = () => {
-        let progress = this.MusicSlider.getComponent(Slider).progress
-        sys.localStorage.setItem('MusicProgress', JSON.stringify(progress))
-        this.audioInstance.adjustMusicSound(progress)
-    }
+  adjustSoundEffect = () => {
+    let progress = this.SoundSlider.getComponent(Slider).progress;
+    sys.localStorage.setItem("SoundProgress", JSON.stringify(progress));
+    this.audioInstance.adjustSoundEffectSound(progress);
+  };
+  displaySettingPanel() {
+    console.log("Active setting panel");
+    this.settingPanel.active = true;
+    // this.settingPanel.setSiblingIndex();
+  }
+  hideSettingPanel() {
+    this.settingPanel.active = false;
+  }
+  start() {
+    console.log("Volumn Adjustment start called");
+  }
 
-    adjustSoundEffect = () => {
-        let progress = this.SoundSlider.getComponent(Slider).progress
-        sys.localStorage.setItem('SoundProgress', JSON.stringify(progress))
-        this.audioInstance.adjustSoundEffectSound(progress)
-    }
-
-    start() {
-
-    }
-
-    update(deltaTime: number) {
-
-    }
+  update(deltaTime: number) {}
 }
-
