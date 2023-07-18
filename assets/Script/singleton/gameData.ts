e18279de39866b8e1233d02ca1c2dd42aac4b112
Mohit Malik <mohit.malik@chicmic.co.in>
2023-07-18 17:10:11 +0530

    Update: Map selection updated

--- assets/Script/singleton/gameData.ts
+++ assets/Script/singleton/gameData.ts
@@ -1,76 +1,87 @@
-import { _decorator, Component, log, Node } from 'cc';
-import { photonmanager } from '../photon/photonmanager';
+import { _decorator, Component, log, Node } from "cc";
+import { photonmanager } from "../photon/photonmanager";
 const { ccclass, property } = _decorator;
 
-@ccclass('gameData')
+@ccclass("gameData")
 export class gameData {
-    private static _instance: gameData = null;
-    private modeIndex: gameData = null;
-    private targetMapNode: Node = null;
-    private SaveUser: string = "";
-    // private UserName: string = "";
-    private gameData() {
+  private static _instance: gameData = null;
+  private modeIndex: gameData = null;
+  private targetMapNode: Node = null;
+  private SaveUser: string = "";
+  // private UserName: string = "";
+  private gameData() {}
 
+  /**
+   *
+   * @returns instance of the class
+   */
+  public static getInstance() {
+    if (!gameData._instance) {
+      gameData._instance = new gameData();
     }
+    return gameData._instance;
+  }
 
-    /**
-     * 
-     * @returns instance of the class
-     */
-    public static getInstance() {
-        if (!gameData._instance) {
-            gameData._instance = new gameData()
-        }
-        return gameData._instance
-    }
+  /**
+   *
+   * @param index It is the index of the mode as defined in enum
+   */
+  initMode(index) {
+    this.modeIndex = index;
+  }
 
-    /**
-     * 
-     * @param index It is the index of the mode as defined in enum
-     */
-    initMode(index) {
-        this.modeIndex = index;
-    }
+  /**
+   *
+   * @returns the mode index which is used in deciding the number of players as per the mode
+   */
+  getModeOnSelect() {
+    return this.modeIndex;
+  }
 
-    /**
-     * 
-     * @returns the mode index which is used in deciding the number of players as per the mode
-     */
-    getModeOnSelect() {
-        return this.modeIndex
-    }
+  initMapWithMaxVotes(targetMapWithMaxVotes) {
+    // console.log("Got map", targetMapWithMaxVotes);
+    console.log(targetMapWithMaxVotes);
 
-    initMapWithMaxVotes(targetMapWithMaxVotes) {
-        // console.log("Got map", targetMapWithMaxVotes);
-        console.log(targetMapWithMaxVotes);
-        photonmanager.getInstance().photon_instance.maps[targetMapWithMaxVotes.name] = photonmanager.getInstance().photon_instance.maps[targetMapWithMaxVotes.name] + 1;
-        photonmanager.getInstance().photon_instance.raiseEvent(2000, { name: targetMapWithMaxVotes.name })
-        this.targetMapNode = targetMapWithMaxVotes
-
-    }
-
-    getMapWithMaxVotes() {
-
-
-
-
-        return this.targetMapNode
-    }
-
-    // SetUserName(UserName){
-    //     this.UserName = UserName
+    // if (this.object[event.target.name] > 0) {
+    //   console.log("Map Inc", this.object[event.target.name]);
+    // } else {
+    photonmanager.getInstance().photon_instance.maps[targetMapWithMaxVotes.name] += 1;
+    //   this.object[event.target.name] = 1;
+    let selectedObj = photonmanager.getInstance().photon_instance.maps[targetMapWithMaxVotes.name];
+    // for (const key in photonmanager.getInstance().photon_instance.maps) {
+    //   //   // console.log("Check Method in Map.ts", this.object[key]);
+    //   if (photonmanager.getInstance().photon_instance.maps[key] == selectedObj) {
+    //   } else {
+    //     if (photonmanager.getInstance().photon_instance.maps[key] > 0) {
+    //       photonmanager.getInstance().photon_instance.maps[key] -= 1;
+    //       //   photonmanager.getInstance().photon_instance.maps[targetMapWithMaxVotes.name] -= 1;
+    //     }
+    //     // photonmanager.getInstance().photon_instance.maps[targetMapWithMaxVotes.name] = 0;
+    //   }
+    //   //   }
     // }
 
-    // GetUserName(){
-    //     return this.UserName
-    // }
+    photonmanager.getInstance().photon_instance.raiseEvent(2000, { name: targetMapWithMaxVotes.name });
+    this.targetMapNode = targetMapWithMaxVotes;
+  }
 
-    SetSaveUserName(UserName) {
-        this.SaveUser = UserName
-    }
+  getMapWithMaxVotes() {
+    return this.targetMapNode;
+  }
 
-    GetSaveUserName() {
-        return this.SaveUser
-    }
+  // SetUserName(UserName){
+  //     this.UserName = UserName
+  // }
+
+  // GetUserName(){
+  //     return this.UserName
+  // }
+
+  SetSaveUserName(UserName) {
+    this.SaveUser = UserName;
+  }
+
+  GetSaveUserName() {
+    return this.SaveUser;
+  }
 }
-
